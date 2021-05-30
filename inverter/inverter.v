5d5dabab25277a82269c7070e696893bab9a5f09
KarthikMB <36357510+Ikarthikmb@users.noreply.github.com>
2021-05-30 22:21:16 +0530

    updated code for errors

--- inverter/inverter.v
+++ inverter/inverter.v
@@ -1,16 +1,23 @@
-module inverter();
-	wire y;
-	reg a = 0;
-	
-	assign y=~a;
-	
-	always
-		#10 a = ~a;
+// Module initialization
+module inverter(
+  input   a,      // input declaration
+  output  y       // output declaration
+);
+  assign y = ~a;  // Assigning output value
+endmodule
 
-	initial begin
-		$monitor("a=%b, y=%b", a, y);
-		$dumpfile("inverter.vcd");
-		$dumpvars(0, inverter);
-	#100 $finish;
-	end
+// Testbench declaration
+module inverter_tb();
+  wire y;
+  reg  a = 0;
+
+  always begin
+    #10 a = ~a;   // assigning a = NOT a for every 10ns
+  end
+
+  initial begin
+    $monitor("a=%d, y=%d", a, y); // Monitoring output
+    $dumpvars("inverter.vcd);     // storing generated values in vsd file
+    $dumpvars(0, inverter);
+  end
 endmodule
